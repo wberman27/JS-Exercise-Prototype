e9dbfac8e31c63e17db34464955d8a86038cd895
Will B <wberman27@gmail.com>
2021-01-27 18:45:10 -0500

    added task 3

--- index.js
+++ index.js
@@ -113,18 +113,35 @@ console.log(lambdaStudentThree.stomach);
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+ 
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
+  };
+  
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
+  };
+  
+  const baby1 = new Baby('Eli', 3, 'airplanes');
+
+  console.log(baby1.play());
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
+    
     1. 
+
     2. 
+
     3. 
+
     4. 
+    
   */
   
   
